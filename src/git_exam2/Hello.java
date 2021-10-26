2b3c626b5765fcf00fe7922dbf4510b1d5c51ea0
kimheeseon <gmltjs418@naver.com>
2021-10-26 10:06:05 +0900

    201914063-1

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,8 +3,8 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("201014099");
-		System.out.println("È«±æµ¿");
+		System.out.println("201914063");
+		System.out.println("±èÈñ¼±");
 	}
 
 }
